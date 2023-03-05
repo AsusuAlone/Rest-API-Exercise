7f58153182c6423b8d73a8f5f90c6c0f2b618cda
Kiki <killian.thm.sio@gmail.com>
2023-03-05 22:47:21 +0100

    add header access to app file

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ app.use(json())
 app.use(morgan("dev"))
 app.use(urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Controll-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Controll-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  )
+  res.setHeader("Access-Controll-Allow-Methods", "GET,POST,PUT,DELETE")
+  next()
+})
+
 makeUserRoutes({ app })
 makeRoleRoutes({ app })
 makePageRoutes({ app })
